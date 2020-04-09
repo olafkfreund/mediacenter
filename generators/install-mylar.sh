dda7a88ae3956dbdb825908be51177975f652510
Olaf K-Freund <olaf.freund@basefarm.com>
2020-04-09 10:12:26 +0000

    small updates.

--- generators/install-mylar.sh
+++ generators/install-mylar.sh
@@ -2,4 +2,4 @@
 export DIR=./descriptors/mylar
 helm repo add k8s-usenet https://raw.githubusercontent.com/aldoborrero/k8s-usenet/master/charts/
 helm repo update
-helm template 0.1 k8s-usenet/mylar > $DIR/00-mylar.yaml
+helm template zak k8s-usenet/mylar > $DIR/00-mylar.yaml
